6099cf9542a278d202c256e735657eec04d2c9b5
mohmed <mmohl9989@gmail.com>
2021-11-23 20:50:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my_bmi/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.my_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView go_to_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        go_to_login = findViewById(R.id.go_to_login);
+
+        go_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
